3509440b1fdf0e1e11fe39e3dd018b04db73d68f
Lvitoria <lucasvitoriaalves@hotmail.com>
2022-02-07 22:43:22 -0300

    add swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Request, response, Response } from 'express'
 import multer from 'multer'
+import swaggerUi from 'swagger-ui-express'
+import swaggerFile from './swagger.json'
+
 const port = 3004
 const app = express()
+
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+    
 const upload = multer({
     dest: './tmp'
 })
@@ -12,7 +19,12 @@ app.post('/upload', upload.single('file'), (req: Request, res: Response)=>{
     res.status(200).send(file)
 })
 
+app.get('/', (req: Request, res: Response)=>{
+    res.status(200).send({ "host": `server ${port}`})
+})
+
+
 
 app.listen(port, ()=>{
-    console.log('server rodando ...')
+    console.log(`server rodando na porta ${port} ... `)
 })
